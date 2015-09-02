639db6ac0627a38991a251a5343c69d7d1bf2de4
cslatton <cslatton@xpanxion.com>
2015-09-02 15:36:19 -0500

    made it a web app

---
+++ src/main/java/com/xpanxion/web/CalculatorServlet.java
@@ -0,0 +1,18 @@
+package com.xpanxion.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CalculatorServlet extends HttpServlet {
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String name= req.getParameter("guest");
+		resp.getWriter().write("Hi, " + name);
+	}
+
+}
